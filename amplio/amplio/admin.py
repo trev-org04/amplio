90394555cf1a5e2dd1d63a2aea654bc9a3335307
trev-org04 <58343522+trev-org04@users.noreply.github.com>
2021-07-15 21:40:46 -0400

    Delete amplio/amplio directory

--- amplio/amplio/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from . import models
-
-class Registration(admin.ModelAdmin):
-    pass
-admin.site.register(models.registration, Registration)
--- amplio/amplio/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class registration(models.Model):
-    name = models.CharField(max_length=50)
-    grade = models.CharField(max_length=10)
-    date = models.CharField(max_length=10)
-    email = models.EmailField(max_length=50)
-    extraInfo = models.TextField(max_length=1000)
