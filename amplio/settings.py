68ee89b5f2aa02dad777f904113ed3e5da5a024a
Trevor <tmgerald04@gmail.com>
2021-07-15 23:11:59 -0400

    database changes

--- amplio/settings.py
+++ amplio/settings.py
@@ -130,8 +130,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# import dj_database_url 
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
 
 django_heroku.settings(locals())
