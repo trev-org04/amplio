98c89767210a53af92f49a081d3a7c8e2fc954a4
Trevor <tmgerald04@gmail.com>
2021-07-15 23:43:49 -0400

    changes

--- amplio/settings.py
+++ amplio/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'krb1kb)g3ha*cjqdpr*#*g_h!)axceoq%3)h006c5j024*paze'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
