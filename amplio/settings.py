3e0153364ed91c1bab4fdc81f873085c1c8006ee
Trevor <tmgerald04@gmail.com>
2021-07-21 22:49:04 -0400

    2nd library trial

--- amplio/settings.py
+++ amplio/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os 
-# import django_heroku
+import django_heroku
 # import dj_database_url
 # import psycopg2
 
@@ -88,9 +88,9 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(prod_db)
+# import dj_database_url
+# prod_db  =  dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(prod_db)
 
 
 # Password validation
@@ -141,4 +141,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # prod_db  =  dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(prod_db)
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
