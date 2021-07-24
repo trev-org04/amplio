468a716663fa7240fd1e3c67b0b447cc235a83ae
Trevor <tmgerald04@gmail.com>
2021-07-23 23:57:55 -0400

    no more webinar form

--- amplio/urls.py
+++ amplio/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('forms/', views.forms, name='mainform'),
-    path('forms/webinar/', views.webinar, name='webinar_form'),
+    # path('forms/webinar/', views.webinar, name='webinar_form'),
     path('forms/waitlist/', views.waitlist, name='waitlist_form'),
     path('admin/', admin.site.urls),
 ]
