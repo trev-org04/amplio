8f701fe5f8471be7455af5e34233dbcab273148f
Trevor <tmgerald04@gmail.com>
2021-08-05 15:23:47 -0400

    removed WN staticfiles storage

--- amplio/settings.py
+++ amplio/settings.py
@@ -88,7 +88,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
